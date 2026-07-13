a88cf915e915b9b63927a1ca76753b65262257e7
wayland123456 <854072590@qq.com>
2026-07-13 09:00:33 +0800

    Add schedule PWA: mobile timetable with class reminders

---
+++ schedule/service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'schedule-pwa-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request).catch(() => caches.match('./index.html'))
+    )
+  );
+});
